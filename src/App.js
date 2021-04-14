5efef207d7fe3cc5a7a2738011719bb577eab553
cubexo <surajgupta19987>
2021-04-14 19:06:25 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import axios from 'axios';
 
-function App() {
+
+
+
+class App extends Component {
+  constructor(props){
+  super(props)
+  this.state={
+    user:[]
+  }
+}
+componentWillUnmount(){
+  axios('https://api.randomuser.me/?nat=us&results=5').than(response=> this.setState({user:response}))
+}
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cubexo</h1>
     </div>
   );
+}
 }
 
 export default App;
